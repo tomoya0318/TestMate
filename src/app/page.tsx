b8f6a5d460c4b224f04a4d08b2b067d3e162c0ee
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-07 02:29:24 +0900

    session data for debag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { auth } from "@/libs/auth";
+
+const Home = async() => {
+  const session = await auth();
   return (
     <>
       <div>Home page</div>
+      <h2>現在のセッションデータ</h2>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </>
   );
-}
+};
+
+export default Home;
