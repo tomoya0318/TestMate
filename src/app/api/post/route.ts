c3ce52883025408add5bfc9aa606966c94317e71
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-08 22:07:55 +0900

    create API for get all post

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/server";
+
+//ポストの全記事取得
+export const GET = async () => {
+  try {
+    const posts = await prisma.post.findMany();
+    return NextResponse.json(posts);
+  } catch (err) {
+    return NextResponse.json({ messege: 'Error', err }, { status: 500 });
+  }
+};
