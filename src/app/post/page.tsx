3f1cbadea62c82d7a596ec1d5e520cb87a6a0fad
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-09 17:10:27 +0900

    create link to description page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,5 +1,6 @@
 import { getAllPost } from "@/api/get-all-post";
 import { PostProps } from "@/types/post";
+import Link from 'next/link';
 import {
   Box,
   Container,
@@ -15,12 +16,14 @@ const Postpage = async() => {
       <Container fontSize={40} textAlign="center">Post</Container>
       {posts.map((post: PostProps) => (
         <Container key={post.id}>
-          <HStack spacing={4}>
-            <Box boxSize="100px">
-              <Image src='https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_14.png' alt="app icon" />
-            </Box>
-            <Box fontSize="xl" fontWeight="bold">{post.title}</Box>
-          </HStack>
+          <Link href={`/post/${post.id}`}>
+            <HStack spacing={4}>
+              <Box boxSize="100px">
+                <Image src='https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_14.png' alt="app icon" />
+              </Box>
+              <Box fontSize="xl" fontWeight="bold">{post.title}</Box>
+            </HStack>
+          </Link>
         </Container>
       ))}
     </VStack>
