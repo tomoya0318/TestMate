00e79265ae1b32606ae06e2d22c847f990aeb28e
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-09 18:22:04 +0900

    add page description

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { getSinglePost } from "@/api/get-single-post";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading } from "@chakra-ui/react";
 
 const PostDescription = async ({ params }: { params: { id: number } }) => {
   const post = await getSinglePost(params.id);
   return (
-    <>
-      <div>{post.title}</div>
-      <div>{post.description}</div>
-    </>
+    <Flex justifyContent="center" alignItems="center">
+      <Box w="100%" maxW="600px" textAlign="center">
+        <Heading mb={4}>{post.title}</Heading>
+        <Text>{post.description}</Text>
+      </Box>
+    </Flex>
   );
 };
 
