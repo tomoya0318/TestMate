08cc1e326894670eb01e969f726ebe659101d085
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-09 17:10:53 +0900

    create get single post API

---
+++ src/app/api/post/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/server";
+
+//ブログ詳細記事取得API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split('/post/')[1]);
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json(post);
+  } catch (err) {
+    return NextResponse.json({ messege: 'Error', err }, { status: 500 });
+  }
+};
